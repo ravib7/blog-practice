81d1fa6d10f34caa279d47e13602d5f34c61f056
ravib7 <ravibchaudhari7@gmail.com>
2025-03-21 15:24:40 +0530

    updated backend with frontend integration

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
 require("dotenv").config()
+const path = require("path")
 
 const app = express()
 
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(express.static("dist"))
+app.use(cors({ origin: "https://blog-practice-4vaq.onrender.com", credentials: true }))
 app.use(cookieParser())
 
 app.use("/api/auth", require("./route/auth.route"))
 
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "resource not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 
 app.use((err, req, res, next) => {
